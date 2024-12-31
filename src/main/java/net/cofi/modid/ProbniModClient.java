325892b7bd0026f52636523ff389064d50136e16
CofiShachou <filipsavic230@gmail.com>
2024-12-31 20:51:53 +0100

    Ajde

---
+++ src/main/java/net/cofi/modid/ProbniModClient.java
@@ -0,0 +1,10 @@
+package net.cofi.modid;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class ProbniModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+ 
+    }
+}
